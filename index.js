db1b652c78062e34a9fe159feb14cc616c8e7a55
AkshatGupta2005 <akshatguptaip@gmail.com>
2024-09-28 22:01:01 +0530

    Complete efficiency increase

--- index.js
+++ index.js
@@ -13,20 +13,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
 app.get("/chapters/:chapterNumber/:lang", async (req, res) => {
   try {
     const chapterNumber = req.params.chapterNumber;
     const language = req.params.lang;
     const response = await axios.get(
-      "https://bhagavad-gita3.p.rapidapi.com/v2/chapters/" + chapterNumber+ "/",
+      "https://bhagavad-gita3.p.rapidapi.com/v2/chapters/" +
+        chapterNumber +
+        "/",
       { headers: headerData }
     );
-    res.render("chapters.ejs", {chapterData: response.data, language: language});
+    res.render("chapters.ejs", {
+      chapterData: response.data,
+      language: language,
+    });
   } catch (error) {
     console.error("Error : ", error.message);
   }
+});
+app.get("/verse/:verseNumber/:language", (req,res) =>{
+  res.render("verse.ejs");
 });
 app.listen(port, () => {
   console.log(`Jai shree krishna, Server active at ${port}`);
